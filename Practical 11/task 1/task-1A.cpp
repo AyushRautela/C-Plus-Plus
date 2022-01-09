93e5f1cab86d8ff4b5214a8cbac0cd32c2c3c9bc
AyushRautela <91423175+AyushRautela@users.noreply.github.com>
2022-01-09 22:15:15 +0530

    Add files via upload

---
+++ Practical 11/task 1/task-1A.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+ 
+class  A{
+
+public:
+int x,y;
+};
+
+class B: public A{
+
+public:
+B(){
+x=10;// due to inheritance ....part a
+y=20;//
+}
+
+void display(){
+cout<<x<<" "<<y<<endl;
+}
+};
+int main()
+{
+B o;//oops
+o.display();
+o.x=30;//inheritance ... part b
+o.y=40;//inheritance
+o.display();//oops
+}
